71e80723714c94d44ceaa4cf966607f06c1e239b
rubelc914 <rubelc914@gmail.com>
2022-08-15 20:32:07 +0600

    use function

--- js/custom.js
+++ js/custom.js
@@ -1,52 +1,65 @@
-document.getElementById('deposit-button').addEventListener('click',function(){
-    
-    // get deposit input field
-    const depositInputFiled = document.getElementById('deposit-input');
-    const depositInputText = depositInputFiled.value;
-    const depositInputAmount = parseFloat(depositInputText);
-    // console.log(depositInputAmount);
-    depositInputFiled.value = '';
-    // get total in deposit display
-    const getDepositTotal = document.getElementById('deposit-total');
-    const getDepositTotalText = getDepositTotal.innerText;
-    const getDepositTotalAmount = parseFloat(getDepositTotalText);
 
-    const totalDepositAmount = getDepositTotalAmount + depositInputAmount;
-    getDepositTotal.innerText = totalDepositAmount;
+function getInputField(inputId){
+    const inputFiled = document.getElementById(inputId);
+    const inputText = inputFiled.value;
+    const InputAmount = parseFloat(inputText);
+    // clear input field 
+    inputFiled.value = '';
+    return InputAmount;
+}
 
-    // get balance update balance
+function getInputTotal(inputAmount,inputTotal){
+    const getPreviousTotal = document.getElementById(inputTotal);
+    const getPreviousTotalText = getPreviousTotal.innerText;
+    const getPreviousTotalAmount = parseFloat(getPreviousTotalText);
+    const totalAmount = getPreviousTotalAmount + inputAmount;
+    getPreviousTotal.innerText = totalAmount;
+}
+function getCurrentBalance(){
     const getBalance = document.getElementById('total-balance');
     const getBalanceText = getBalance.innerText;
     const getBalanceAmount = parseFloat(getBalanceText);
-    const totalBalanceAmount = getBalanceAmount + depositInputAmount;
-    getBalance.innerText = totalBalanceAmount;
+    return getBalanceAmount;
+}
+
+function updateTotalBalance(inputAmount,isAdd){
+    const getBalance = document.getElementById('total-balance');
+    // const getBalanceText = getBalance.innerText;
+    // const getBalanceAmount = parseFloat(getBalanceText);
+    const getBalanceAmount = getCurrentBalance();
+    if(isAdd == true){
+        const totalBalanceAmount = getBalanceAmount + inputAmount;
+        getBalance.innerText = totalBalanceAmount;
+    }else{
+        const totalBalanceAmount = getBalanceAmount - inputAmount;
+        getBalance.innerText = totalBalanceAmount;
+
+    }
+}
+
+document.getElementById('deposit-button').addEventListener('click',function(){
+
+    const depositInputAmount = getInputField('deposit-input');
+       if(depositInputAmount > 0){
+        
+        getInputTotal(depositInputAmount,'deposit-total');
+        updateTotalBalance(depositInputAmount,true);
+    }
+
+   
 });
 
 
 document.getElementById('withdraw-button').addEventListener('click',function(){
-    // get withdraw amount 
-    const withdrawInputField = document.getElementById('withdraw-input');
-    const withdrawInputText = withdrawInputField.value;
-    const withdrawInputAmount = parseFloat(withdrawInputText);
-    //clear withdraw amount 
-    withdrawInputField.value = '';
-
-    //get total withdraw display
-    const getWithdrawTotal = document.getElementById('withdraw-total');
-    const getWithdrawTotalText = getWithdrawTotal.innerText;
-    const getWithdrawTotalAmount = parseFloat(getWithdrawTotalText);
    
-    const totalWithdrawAmount = getWithdrawTotalAmount + withdrawInputAmount;
-    getWithdrawTotal.innerText = totalWithdrawAmount;
+    const withdrawInputAmount = getInputField('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if(withdrawInputAmount > 0 &&  withdrawInputAmount < currentBalance){
 
-    //get total balance update
-    const getBalance = document.getElementById('total-balance');
-    const getBalanceText = getBalance.innerText;
-    const getBalanceAmount = parseFloat(getBalanceText);
-    console.log(withdrawInputAmount);
-
-    const totalBalanceAmount= getBalanceAmount - withdrawInputAmount;
-    getBalance.innerText =totalBalanceAmount;
-
+        getInputTotal(withdrawInputAmount,'withdraw-total');
+        updateTotalBalance(withdrawInputAmount,false);
+    }
+    
+    
 
 });
